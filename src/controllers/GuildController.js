f7b39a5a7b4aa9b011925c1d4750d96420529614
Eman134 <kaykeeman@gmail.com>
2022-11-05 17:48:31 -0300

    Mutual guilds

--- src/controllers/GuildController.js
+++ src/controllers/GuildController.js
@@ -1,13 +1,11 @@
-const { getBotGuildsService, getUserGuildsService } = require("../services/guilds")
+const { getMutualGuildsService } = require("../services/guilds")
 
 module.exports = {
     async index(req, res) {
 
         try {
-            console.log(req.user)
-            const botGuilds = await getBotGuildsService()
-            const userGuilds = await getUserGuildsService(req.user.access_token)
-            res.send({ botGuilds, userGuilds })
+            const botGuilds = await getMutualGuildsService(req.user.access_token)
+            res.send(botGuilds)
         } catch (error) {
             console.log(error)
             res.status(500).send({ msg: 'Internal Server Error' })
--- src/services/guilds/index.js
+++ src/services/guilds/index.js
@@ -1,21 +1,32 @@
 const axios = require('axios');
 const { DISCORD_API_URL } = require('../../utils/constants');
 
+async function getBotGuildsService() {
+    const response = await axios.get(`${DISCORD_API_URL}/users/@me/guilds`, {
+        headers: {
+            Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
+        }
+    });
+    return response.data;
+}
+async function getUserGuildsService(access_token) {
+    const response = await axios.get(`${DISCORD_API_URL}/users/@me/guilds`, {
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    });
+    return response.data;
+}
+async function getMutualGuildsService(access_token) {
+    const botGuilds = await getBotGuildsService()
+    const userGuilds = await getUserGuildsService(access_token)
+    const adminGuilds = userGuilds.filter(guild => guild.permissions & 8)
+    const mutualGuilds  = adminGuilds.filter(guild => botGuilds.some(botGuild => botGuild.id === guild.id))
+    return { adminGuilds, mutualGuilds };
+}
+
 module.exports = {
-    async getBotGuildsService() {
-        const response = await axios.get(`${DISCORD_API_URL}/users/@me/guilds`, {
-            headers: {
-                Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
-            }
-        });
-        return response.data;
-    },
-    async getUserGuildsService(access_token) {
-        const response = await axios.get(`${DISCORD_API_URL}/users/@me/guilds`, {
-            headers: {
-                Authorization: `Bearer ${access_token}`
-            }
-        });
-        return response.data;
-    }
+    getBotGuildsService,
+    getUserGuildsService,
+    getMutualGuildsService
 }
